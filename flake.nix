74f7cbaccc8c8bf0185456ef760ad504445f9f7e
Andrea Frigido <frisoft@users.noreply.github.com>
2025-06-07 20:57:12 +0100

    Add flake.nix and .envrc for zola

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Kind Approach website development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zola
+          ];
+
+          shellHook = ''
+            echo "Kind Approach development environment"
+            echo "Zola version: $(zola --version)"
+          '';
+        };
+      });
+}
